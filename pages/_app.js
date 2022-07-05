b4c65a193f87bca62fa2f6bba8a2f14d7da185cb
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-07-05 17:05:55 +0100

    test: react page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Loading() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const handleStart = (url) => {
+      url !== router.asPath && setLoading(true);
+    };
+    const handleComplete = (url) => {
+      url === router.asPath &&
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
+    };
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+  return (
+    loading && (
+      <div className="spinner-wrapper">
+        <div className="spinner" />
+      </div>
+    )
+  );
+  //routeChangeStart
+  //routeChangeComplete
+  //routeChangeError
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Loading />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/page.js
@@ -0,0 +1,47 @@
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+import { PageTransition } from "@steveeeie/react-page-transition";
+import { useState } from "react";
+
+export default function Page() {
+  const [contador, setContador] = useState(0);
+
+  function contar() {
+    console.log("teste");
+    setContador(contador + 1);
+  }
+
+  return (
+    <div>
+      this is a page
+      <Square>
+        <PageTransition preset={"foldTopFromBottom"} transitionKey={contador}>
+          {contador % 2 === 0 ? (
+            <Square2 onClick={contar}>AAA</Square2>
+          ) : (
+            <Square2 onClick={contar}>BBB</Square2>
+          )}
+        </PageTransition>
+      </Square>
+    </div>
+  );
+}
+
+const Square = styled.div`
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  overflow: hidden;
+  padding: 50px;
+  background-color: grey;
+  height: 800px;
+  width: 800px;
+`;
+
+const Square2 = styled.div`
+  background-color: azure;
+  height: 700px;
+  width: 700px;
+`;
